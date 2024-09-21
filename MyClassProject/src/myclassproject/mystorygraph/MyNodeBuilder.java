8b841426d7246ac445d0b48e71f6fa4f1b17e741
saravannoni <saravannoni@localhost>
2024-09-21 16:06:06 -0500

    sara test

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
@@ -1,18 +1,31 @@
 package myclassproject.mystorygraph;
 
 import static myclassproject.questexample.QuestStoryEntities.bandit;
+import static myclassproject.questexample.QuestStoryEntities.cityDoor;
 import static myclassproject.questexample.QuestStoryEntities.cottage;
+import static myclassproject.questexample.QuestStoryEntities.cottageDoor;
 import static myclassproject.questexample.QuestStoryEntities.player;
 import static myclassproject.questexample.QuestStoryEntities.sword;
 import static myclassproject.questexample.QuestStoryEntities.town;
 
 import java.util.List;
 
+import com.actions.DisableInput;
 import com.actions.Draw;
+import com.actions.EnableInput;
+import com.actions.Enter;
+import com.actions.Exit;
 import com.actions.Face;
+import com.actions.FadeOut;
+import com.actions.HideDialog;
+import com.actions.HideNarration;
+import com.actions.Pocket;
 import com.actions.SetPosition;
+import com.actions.Take;
 import com.sequences.CreateAll;
 import com.sequences.CreateCharacterSequence;
+import com.sequences.DialogSequence;
+import com.sequences.NarrationSequence;
 import com.storygraph.*;
 
 import myclassproject.questexample.NodeLabels;
@@ -107,38 +120,67 @@ public class MyNodeBuilder extends NodeBuilder {
 	public void EatChickenRouteAActions() {
 
 	}
+	
+	
+	//Sara Vannoni
+	
 	@BuilderMethod
 	public void EatChickenRouteBActions() {
-
+		var node = get(NodeLabels.EatChickenRouteB.toString));
+		
+//		Eat Chicken -> bad ending death
 	}
+	
 	@BuilderMethod
 	public void BadEndingRouteAActions() {
-
+		var node = get(NodeLabels.BadEndingRouteA.toString());
+		node.add(new NarrationSequence("You take a bite of chicken. It tastes like cleaning supplies. Oh no!"));
+//		node.add(new HideDialog()).add(new NarrationSequence("You take a bite of chicken. It tastes like cleaning supplies. Oh no!"));
+		node.add(new HideNarration()).add(new FadeOut());
+		//		Player dies from eating chicken (early entry to kitchen)
 	}
 	@BuilderMethod
 	public void BadEndingRoute1BActions() {
-
+		var node = get(NodeLabels.BadEndingRoute1B.toString());
+		node.add(new NarrationSequence("You take a bite of chicken. It tastes like cleaning supplies. Oh no!"));
+		node.add(new HideNarration()).add(new FadeOut());
+		//		Player dies from eating chicken (2nd time entry)
 	}
 	@BuilderMethod
 	public void BadEndingRoute2BActions() {
-
+		var node = get(NodeLabels.BadEndingRoute2B.toString());
+		node.add(new NarrationSequence("You turn away to continue investigating and feel yourself get stabbed! Oh no!"));
+		node.add(new HideNarration()).add(new FadeOut());
+		//		Player dies because he believed chef
 	}
+
 	@BuilderMethod
 	public void FindSkullActions() {
-
-	}
+		var node = get(NodeLabels.FindSkull.toString());
+		node.add(new DialogSequence(player, chef, List.of("What do you do?"),
+				List.of("Believe Chef", "Arrest Chef")));
+//		Player investigates and picks up a skull
+	}	
 	@BuilderMethod
 	public void ArrestChefActions() {
-
+		var node = get(NodeLabels.ArrestChef.toString());
+		node.add(new NarrationSequence("You handcuff the Chef and take him into custody."));
+//		Player arrests the chef
 	}
+
 	@BuilderMethod
 	public void BelieveChefActions() {
-
+		var node = get(NodeLabels.BelieveChef.toString());
+		node.add(new HideDialog()).add(new NarrationSequence("The player says 'Ok! I beleive you.'"));
+//		Player says that he believes chef
 	}
+
 	@BuilderMethod
 	public void GoodEndingActions() {
-
+		var node = get(NodeLabels.GoodEnding.toString());
+		node.add(new HideNarration()).add(new NarrationSequence("The city police reward you with 10 million gold coins for solving the mystery! You can retire in peace."));
+		node.add(new HideNarration()).add(new FadeOut());
+		//		Player wins!
 	}
 
-	
-	//Sara Vannoni
+	}
